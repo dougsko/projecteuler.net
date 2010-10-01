4d818addfd2eb28376783f10fd85955c94fefadc
dougsko <dougtko@gmail.com>
2010-10-01 17:01:30 -0400

    working on 23

---
+++ 23/solution.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+#
+# projecteuler.net
+#
+# problem 23
+#
+# A perfect number is a number for which the sum of its proper divisors is
+# exactly equal to the number. For example, the sum of the proper divisors of 28
+# would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+#
+# A number n is called deficient if the sum of its proper divisors is less than
+# n and it is called abundant if this sum exceeds n.
+#
+# As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest
+# number that can be written as the sum of two abundant numbers is 24. By
+# mathematical analysis, it can be shown that all integers greater than 28123
+# can be written as the sum of two abundant numbers. However, this upper limit
+# cannot be reduced any further by analysis even though it is known that the
+# greatest number that cannot be expressed as the sum of two abundant numbers is
+# less than this limit.
+#
+# Find the sum of all the positive integers which cannot be written as the sum
+# of two abundant numbers.
+#
+
+def factors(num)
+    i = 1
+    second_fact = 0
+    facts = [i]
+    2.upto Math.sqrt(num) do |i|
+        if num % i == 0
+            facts << i
+            if i != 1 && i != 0
+                second_fact = num / i
+                if second_fact != num
+                    facts << second_fact
+                end
+            end
+        end
+    end
+    facts.uniq
+end
+
+def sum_array(array)
+    sum = 0
+    array.collect{|x| sum += x}
+    sum
+end
+
+abundants = []
+1.upto 28123 do |n|
+    sum = sum_array(factors(n))
+    if sum > n
+        abundants << n
+    end
+end
+
+all = []
+sum = 0
+1.upto 28123 do |n|
+    abundants.each do |abundant|
+        test = n - abundant
+        if test > 1
+            if ! abundants.include?(test)
+                sum += n
+            end
+        end
+    end
+end
+puts sum
