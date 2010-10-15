2887140591b0c351a1a95b9737a44697a1f40110
dougsko <dougtko@gmail.com>
2010-10-15 16:22:45 -0400

    working on 80

---
+++ 80/solution.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+#
+# projecteuler.net
+#
+# problem 80
+#
+# It is well known that if the square root of a natural number is not an
+# integer, then it is irrational. The decimal expansion of such square
+# roots is infinite without any repeating pattern at all.
+#
+# The square root of two is 1.41421356237309504880..., and the digital
+# sum of the first one hundred decimal digits is 475.
+#
+# For the first one hundred natural numbers, find the total of the
+# digital sums of the first one hundred decimal digits for all the
+# irrational square roots.
+#
+
+require 'bigdecimal'
+
+sum = 0
+1.upto 100 do |i|
+    i = 2
+    array = BigDecimal.new(i.to_s).sqrt(100).to_s('F').split('.')[1].split('')
+    if array != ['0']
+        array[0..99].collect{ |j| sum += j.to_i}
+    end
+    puts sum
+    exit
+end
+puts sum
