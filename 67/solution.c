1d52fa13f8dbdb7726a5d1222413a9bd153ebfd9
Doug P <dougtko@gmail.com>
2010-10-19 07:31:56 -0400

    working on 67

--- 67/solution.c
+++ 67/solution.c
@@ -25,66 +25,68 @@ read_data(gchar *filename)
     rows = g_strsplit(contents, "\n", -1);
     for(i=0; i < g_strv_length(rows); i++)
     {
-        g_ptr_array_add(tri_data,rows[i]);
+        row_array = g_array_new(FALSE, FALSE, sizeof (gint));
+        elements = g_strsplit(rows[i], " ", -1);
+        for(j=0; j < g_strv_length(elements); j++)
+        {
+            k = atoi(elements[j]);
+            g_array_append_val(row_array, k);
+            //g_ptr_array_add(tri_data,rows[i]);
+        }
+        g_ptr_array_add(tri_data,row_array);
     }
     
+    /*
     g_free(elements);
     g_free(rows);
     g_free(contents);
     g_free(row_array);
-
+    */
     return tri_data;
 }
 
 int 
 main(int argc, char **argv)
 {
-    GArray *row;
+    GArray *rows, *row;
     GPtrArray *triangle;
-    gint count, i, j, index, sum;
-    gint biggest_sum = 0;
-    gchar **elements;
+    gint count, i, j, k, m, index, sum;
+    gint biggest_sum = 6594;
 
-    triangle = read_data("triangle_small.txt");
+    triangle = read_data("triangle.txt");
+    row = g_array_new (FALSE, FALSE, sizeof (gint));
     
-    for(i=0; i < triangle->len; i++)
+    for(m=0; m <= 1000000; m++)
     {
-        elements = g_strsplit(g_ptr_array_index(triangle, i), " ", -1);
-        g_print("%d\n", g_strv_length(elements));
-    }
-    return 0;
-    
-    //while(biggest_sum <= 0)
-    for(j=0; j <= 10000; j++)
+    for(k=0; k <= 1000000; k++)
     {
+        //srand(time(NULL));
         index = 0;
         row = g_ptr_array_index(triangle, 0);
         sum = g_array_index(row, gint, 0);
 
-        for(i=1; i < triangle->len; i++)
+        for(i=1; i < triangle->len - 1; i++)
         {
             row = g_ptr_array_index(triangle, i);
-            //print_array(row);
             if(rand() % 2)
             {
-                //g_print("%d += %d\n", sum, g_array_index(row, gint, index));
                 sum += g_array_index(row, gint, index);
             }
             else
             {
-                index += 1;
-                //g_print("%d += %d\n", sum, g_array_index(row, gint, index));
+                index++;
                 sum += g_array_index(row, gint, index);
             }
-        }
-        if(sum > biggest_sum)
-        {
-            biggest_sum = sum;
-            //g_print("%d\n", biggest_sum);
+            if(sum > biggest_sum)
+            {
+                biggest_sum = sum;
+                g_print("%d\n", biggest_sum);
+            }
         }
     }
+    }
     //g_print("%d\n", biggest_sum);
-
+    
     g_ptr_array_free(triangle, TRUE);
     g_array_free(row, TRUE);
 
