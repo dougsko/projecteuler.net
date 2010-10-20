444fb47717f5f681ea11b73a753f4410a4ee9bd0
Doug P <dougtko@gmail.com>
2010-10-19 20:51:58 -0400

    compile.sh

--- 67/compile.sh
+++ 67/compile.sh
@@ -2,8 +2,8 @@
 
 #gcc -g `pkg-config --cflags --libs glib-2.0` -lm -I../tools/ solution.c -o solution
 
-gcc `pkg-config --cflags glib-2.0` -I../tools -I/usr/local/include -Wall -g  -c -o ga_solution.o ga_solution.c
+gcc `pkg-config --cflags glib-2.0` -I../tools -I/usr/local/include -Wall -g  -c -o ga_solution.o ga_solution.c && \
 
-gcc `pkg-config --cflags --libs glib-2.0` -I../tools  -I/usr/local/include -Wall -g -O2  -L/usr/local/lib/ -o ga_solution ga_solution.o -lgaul -lgaul_util -lm -lpthread -lpthread -lgaul -lgaul_util
+gcc `pkg-config --cflags --libs glib-2.0` -I../tools  -I/usr/local/include -Wall -g -O2  -L/usr/local/lib/ -o ga_solution ga_solution.o -lgaul -lgaul_util -lm -lpthread -lpthread && \
 
 rm ga_solution.o
