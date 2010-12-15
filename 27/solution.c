1626b4e2ffb30e8e28f36dbf69d65e96ed2d6261
Doug P <dougtko@gmail.com>
2010-12-15 10:38:37 -0500

    ...

--- 27/solution.c
+++ 27/solution.c
@@ -1,23 +1,17 @@
 #include "../tools/helpers.h"
 
 gint
-run_it(gint n, gint a, gint b)
-{
-    gint res;
-    res = n * n + a * n + b;
-    return res;
-}
-
-gint
 main()
 {
     gint n, old_n, a, b = 0;
 
+    omp_set_num_threads(4);
+    #pragma omp parallel for private(n)
     for(a = -1000; a <= 1000; a++)
     {
         for(b = -1000; b <= 1000; b++)
         {
-            while(is_prime(run_it(n, a, b)))
+            while(is_prime(n * n + a * n + b))
                 n++;
             if(n > old_n)
             {
@@ -27,5 +21,6 @@ main()
             n = 0;
         }
     }
+    return 0;
 }
 
