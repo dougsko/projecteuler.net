2eff575ab7b87c902225c25c90f218d48daa2c19
dougsko <dougtko@gmail.com>
2010-12-30 16:00:24 -0500

    ...

--- 59/solution.rb
+++ 59/solution.rb
@@ -43,73 +43,21 @@ File.open('cipher1.txt', 'r') do |f|
     text_a << f.readlines
 end
 
-text_a = text_a.join(',').split(',')
+text_a = text_a.join(',').chomp.split(',')
 text_a_small = text_a[0..299]
 
 # lower case letters
-# 61 - 7a hex
 # 97 - 122 base10
+# 32 is a space
 
 # puts 65.to_s(16)
-# puts [65.to_s(16)].pack("H*")
+# puts 65.chr
 
-def check_word(array)
-    test = ""
-    winner = "con"
-    array.each do |num|
-       test += [num.to_s(16)].pack('H*')
-    end
-    return true if test == winner
-    return false
-end    
-
-# To test:
-# 103 111 100
-# 100 103 111
-#
-def get_result(text)
-    sum = 0
-    #key = [103, 110, 121]
-    key = [97, 110, 103]
-    decrypted = []
-    paragraph = []
-    0.upto(text.size - 2) do |i|
-        decrypted << (text[i].to_i ^ key[0])
-        decrypted << (text[i+1].to_i ^ key[1])
-        decrypted << (text[i+2].to_i ^ key[2])
-        i += 3
-    end
-    decrypted.collect{|letter| paragraph << [letter.to_s(16)].pack("H*")}
-    decrypted.collect{|letter| sum += letter}
-    puts sum
-    puts paragraph.join('')
-end
-
-#get_result(text_a)
-#exit
-
-freq = {}
-word_s = ""
 97.upto(122) do |a|
     97.upto(122) do |b|
         97.upto(122) do |c|
-            0.upto(text_a.size - 2) do |i|
-                x = text_a[i].to_i
-                y = text_a[i+1].to_i
-                z = text_a[i+2].to_i
-                word = [a ^ x, b ^ y, c ^ z]
-                if check_word(word)
-                    #puts "#{a} #{b} #{c}"
-                    if freq["#{a} #{b} #{c}"] == nil
-                        freq["#{a} #{b} #{c}"] = 1
-                    else
-                        freq["#{a} #{b} #{c}"] += 1
-                    end
-                end
-                i += 3
-            end
+            key = [a, b, c]
         end
-        puts freq.sort{|aa,bb| aa[1] <=> bb[1]}.last.inspect
     end
 end
 
