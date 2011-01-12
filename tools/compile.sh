74ac7da08e495c123efcad189f03241ff1dc26e4
Doug P <dougtko@gmail.com>
2011-01-12 10:26:53 -0500

    ffi

--- tools/compile.sh
+++ tools/compile.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-gcc -g `pkg-config --cflags --libs glib-2.0` -fopenmp -lgmp -lm -I../tools/ ../tools/helpers.o solution.c -o solution
+#gcc -g `pkg-config --cflags --libs glib-2.0` -fopenmp -lgmp -lm -I../tools/ ../tools/helpers.o solution.c -o solution
 #gcc -g `pkg-config --cflags --libs glib-2.0` -fopenmp -lm -lgmp -I. helpers.o test.c -o test
+
+gcc -Wall -fPIC -c `pkg-config --cflags --libs glib-2.0` -fopenmp -lm \
+    -lgmp -I. helpers.c
+gcc -shared -Wl,-soname,libhelpers.so -o libhelpers.so helpers.o
