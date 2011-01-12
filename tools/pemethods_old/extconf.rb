51327dad9339b301a31f19624444c2255703567f
dougsko <dougtko@gmail.com>
2011-01-12 15:56:44 -0500

    moved original pe_methods to their own folder

---
+++ tools/pemethods_old/extconf.rb
@@ -0,0 +1,15 @@
+# Loads mkmf which is used to make makefiles for Ruby extensions
+require 'rubygems'
+require 'mkmf'
+
+$CPPFLAGS << "-I/usr/local/include -lgmp -fopenmp `pkg-config --cflags glib-2.0`"
+dir_config(ENV['PWD'])
+
+have_header('math.h')
+have_header('gmp.h')
+have_header('helpers.h')
+
+have_library('glib-2.0', 'g_array_new')
+have_library('gmp')
+
+create_makefile('pemethods')
