8817f2ace6d8567a28a254472fd68953f4642011
Doug P <dougtko@gmail.com>
2011-03-16 23:25:42 -0400

    working on 205

---
+++ 205/solution.c
@@ -0,0 +1,42 @@
+#include "../tools/pe.h"
+#include <time.h>
+
+
+gint
+roll(num_dice, num_sides)
+{
+    gint sum, i;
+
+    sum = 0;
+    for(i = 0; i < num_dice; i++)
+    {
+        sum += (rand() % num_sides) + 1;
+    }
+    return sum;
+}
+    
+    
+gint
+main()
+{
+    gint i, j, peter, colin, win, total;
+
+    peter, colin, total = 0;
+    srand(time(NULL));
+
+    win = 0;
+    total = 0;
+
+    for(i = 0; i <= 9000000000; i++)
+    {
+        peter = roll(9, 4);
+        colin = roll(6, 6);
+        if(peter > colin)
+            win++;
+        total++;
+    }
+    printf("Wins: %d\tTotal: %d\n", win, total);
+    printf("Win percentage = %0.7f\n", (float)win / total);
+
+    return 0;
+}
