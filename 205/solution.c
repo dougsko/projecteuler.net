6732fb94ed5c6f664b905cf6760499a55cb50098
Doug P <dougtko@gmail.com>
2011-04-04 17:06:20 -0400

    ...

--- 205/solution.c
+++ 205/solution.c
@@ -19,27 +19,41 @@ roll(num_dice, num_sides)
 gint
 main()
 {
-    gint i, j, peter, colin, win, total;
+    gint i, j, peter, colin;
+    mpf_t win, total, res;
+    mp_exp_t expptr;
+    gchar *win_s, *total_s, *res_s;
 
-    peter, colin, total = 0;
+    mpf_init2(win, 256);
+    mpf_init2(total, 256);
+    mpf_init2(res, 7);
+    peter, colin = 0;
     srand(time(NULL));
 
-    win = 0;
-    total = 0;
-
-    for(i = 0; i <= 1000000; i++)
+    // good for 4 decimals so far
+    // 0.57315066
+    // 0.57314323
+    for(i = 0; i <= 10000000; i++)
     {
-        for(j = 0; j <= 1000000; j++)
+        for(j = 0; j <= 10000000; j++)
         {
             peter = roll(9, 4);
             colin = roll(6, 6);
             if(peter > colin)
-                win++;
-            total++;
+                mpf_add_ui(win, win, 1);
+            mpf_add_ui(total, total, 1);
         }
     }
-    printf("Wins: %d\tTotal: %d\n", win, total);
-    printf("Win percentage = %0.7f\n", (float)win / total);
+    mpf_div(res, win, total);
+    win_s = mpf_get_str(NULL, &expptr, 10, 0, win);
+    total_s = mpf_get_str(NULL, &expptr, 10, 0, total);
+    res_s = mpf_get_str(NULL, &expptr, 10, 8, res);
+    printf("Wins: %s\tTotal: %s\n", win_s, total_s);
+    printf("Win percentage = 0.%s\n", res_s);
+
+    mpf_clear(win);
+    mpf_clear(total);
+    mpf_clear(res);
 
     return 0;
 }
---
+++ 92/solution.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+#
+# projecteuler.net
+#
+# problem 92
+#
+# A number chain is created by continuously adding the square of the
+# digits in a number to form a new number until it has been seen before.
+#
+# For example,
+#
+# 44  32  13  10  1  1
+# 85  89  145  42  20  4  16  37  58  89
+#
+# Therefore any chain that arrives at 1 or 89 will become stuck in an
+# endless loop. What is most amazing is that EVERY starting number will
+# eventually arrive at 1 or 89.
+#
+# How many starting numbers below ten million will arrive at 89?
+#
+require '../tools/ffi_pe'
+
+count = 0
+
+# 8581145 - no good
+1.upto(10000000-1) do |j|
+    i = j
+    until i == 89 or i == 1 do
+        sum = 0
+        i.to_s.split('').collect{|x| sum += x.to_i*x.to_i}
+        i = sum
+        #puts i
+        if i == 89
+            count += 1
+        end
+    end
+    #puts j
+    if j % 100000 == 0
+        puts "#{(j / 10000000.0)*100}% done..."
+        puts "count = #{count}"
+    end
+end
+puts "Final count = #{count}"
+
+
