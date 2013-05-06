557d72dbb22c025a99305a749f724ad05da52b61
Doug P <dougtko@gmail.com>
2013-05-06 10:28:10 -0400

    ...

--- 206/solution.m
+++ 206/solution.m
@@ -4,10 +4,8 @@ end
 
 parfor i = 1000000000 : 5000000000
     square = int2str(i * i);
-    if(size(square,2) == 19)
-        if(strcmp(square(1:2:end), '1234567890'))
-            disp(i)
-            exit
-        end
+    if(strcmp(square(1:2:end), '1234567890'))
+        disp(i)
+        exit
     end
 end
