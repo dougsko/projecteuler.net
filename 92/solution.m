10ce04959d9559534d41164a01b8d632a7bc6605
Doug P <dougtko@gmail.com>
2013-08-15 10:31:59 -0400

    ...

--- 92/solution.m
+++ 92/solution.m
@@ -1,15 +1,15 @@
-% 1:10^7 = 8581146
+% i get 85744333, but apparently that's wrong
+
 if( matlabpool('size') <= 0 )
 	matlabpool local 3
 end
 
 clear
+total = 0;
 start = tic();
-spmd
-	%x = labindex:numlabs:31;
-	x = 10^7+1:10^8;
-	x_d = codistributed(x);
-	y = arrayfun(@chain, x_d);
-	size(y(y==89), 2)
+parfor x = 1:10^8
+	y = arrayfun(@chain, x);
+	total = total + size(y(y==89), 2);
 end
+total
 elapsed = toc(start)
